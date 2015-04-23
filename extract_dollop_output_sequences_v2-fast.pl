771898a7c30c176aaf0522bdf249531f71c720d0
Guy Leonard <guy.leonard@gmail.com>
2015-04-23 14:44:06 +0100

    there was a bit of a bbig bug in the convert_to_phylip_style() that caused one of the sets of five states to be missed, I have mostly rewritten it and it now works correctly, more to come...

--- extract_dollop_output_sequences_v2-fast.pl
+++ extract_dollop_output_sequences_v2-fast.pl
@@ -4,6 +4,7 @@ use warnings;
 
 use Cwd;               # Gets pathname of current working directory
 use File::Basename;    # Remove path information and extract 8.3 filename
+use Data::Dumper;
 
 our $WORKING_DIR = getcwd;
 
@@ -16,27 +17,32 @@ our $WORKING_DIR = getcwd;
 # This script is actually going to first export the information about ancestral states
 # as a phylip-style file - which is infinitely easier to parse!
 
+our $EMPTY = q{};
+
 # Files
-my $dollop_outfile = "orthomcl_50_taxa_topology_1.outfile";
-my $groups_files   = "orthomcl_groups_list.txt";
+my $dollop_outfile = "/home/cs02gl/Dropbox/projects/AGRP/jeremy_test/six_genomes/test_orthomcl_pipeline_6_genomes/outfile.old";
+my $groups_files   = "/home/cs02gl/Dropbox/projects/AGRP/jeremy_test/six_genomes/test_orthomcl_pipeline_6_genomes/group_list.txt";
 
 # User Variables
-my $number_of_states = "50453";
+my $number_of_states = "9881";
 
 # Other Variables - Do not change
-my $concat_line          = "";
+#my $concat_line          = "";
 my $number_of_char_lines = int( $number_of_states / 40 );
-my $count                = 0;
+print "$number_of_char_lines\n";
+my $count = 0;
 
-#&convert_to_phylip_style($dollop_outfile);
+&convert_to_phylip_style($dollop_outfile);
+
 #&report_counts("$dollop_outfile\.phy");
 
 my @groups = get_groups("$groups_files");
 
 #&get_group_from_position("$dollop_outfile\.phy");
 
-our $ALIGNMENTS_DIR = "/home/cs02gl/Dropbox/projects/gain_loss/orthomcl/alignments";
-get_alignments_for_group();
+our $ALIGNMENTS_DIR = "/home/cs02gl/Dropbox/projects/AGRP/jeremy_test/six_genomes/test_orthomcl_pipeline_6_genomes/alignments";
+
+#get_alignments_for_group();
 
 sub get_group_from_position {
 
@@ -58,7 +64,7 @@ sub get_group_from_position {
         my @split_line = split( /[\s\t]/, $line );
         my $node       = $split_line[0];
         my $results    = $split_line[1];
-        $results       =~ s/yes//g;
+        $results =~ s/yes//g;
 
         # split the results into chars - each char is a position within the
         # groups array...
@@ -115,19 +121,19 @@ sub get_alignments_for_group {
     foreach my $x (@node_subdirs) {
 
         print "Working on $x\n";
-        
+
         # Test for directory
-        if (-d $x) {
+        if ( -d $x ) {
 
             ## Gains
             #
             # Make a directory for alignments to be placed
             mkdir "$x\/gain" unless -d "$x\/gain";
             print "\tMaking gain directory\n\tCopying";
-            
+
             # Read in file contents to array - it should only be one line.
             open my $gain_in, '<', "$x\/gain\.txt";
-            my @gain_line = split (/\s+/, <$gain_in>);
+            my @gain_line = split( /\s+/, <$gain_in> );
             close($gain_in);
 
             # Foreach array element, get the corresponding file
@@ -143,10 +149,10 @@ sub get_alignments_for_group {
             # Make a directory for alignments to be placed
             mkdir "$x\/loss" unless -d "$x\/loss";
             print "\tMaking loss directory\n\tCopying";
-            
+
             # Read in file contents to array - it should only be one line.
             open my $loss_in, '<', "$x\/loss\.txt";
-            my @loss_line = split (/\s+/, <$loss_in>);
+            my @loss_line = split( /\s+/, <$loss_in> );
             close($loss_in);
 
             # Foreach array element, get the corresponding file
@@ -163,7 +169,7 @@ sub get_alignments_for_group {
             # Make a directory for alignments to be placed
             #mkdir "$x\/core" unless -d "$x\/core";
             #print "\tMaking core directory\n\tCopying";
-            
+
             # Read in file contents to array - it should only be one line.
             #open my $core_in, '<', "$x\/core\.txt";
             #my @line = split (/\s+/, <$core_in>);
@@ -206,78 +212,81 @@ sub report_counts {
         chomp($line);
         next if ( $line =~ m/^\s+/ );
 
-        my @line_array = split (/\t/, $line);
+        my @line_array = split( /\t/, $line );
 
-        my @nodes = split (/\_\_/, $line_array[0]);
+        my @nodes = split( /\_\_/, $line_array[0] );
 
-            # Count number of '.', '0', and '1' in concat_line.
-            # '.' = shared, '0' = loss, '1' = gain
-            my $dot_count  = () = $line_array[1] =~ m/[\.]/g;
-            my $one_count  = () = $line_array[1] =~ m/[1]/g;
-            my $zero_count = () = $line_array[1] =~ m/[0]/g;
+        # Count number of '.', '0', and '1' in concat_line.
+        # '.' = shared, '0' = loss, '1' = gain
+        my $dot_count  = () = $line_array[1] =~ m/[\.]/g;
+        my $one_count  = () = $line_array[1] =~ m/[1]/g;
+        my $zero_count = () = $line_array[1] =~ m/[0]/g;
 
         print $report "$nodes[0]\t$nodes[1]\t$dot_count\t$zero_count\t$one_count\n";
     }
 }
 
 sub convert_to_phylip_style {
-    my $dollop_outfile = shift;
 
-    #print "States:\t$number_of_states\n";
-    #print "Node 1\tNode2\tPrevious\tLoss\tGain\n";
-    print "\tParsing\n";
+    my $dollop_outfile = shift;
+    my $concat_line    = $EMPTY;
+
+    print "Parsing: $dollop_outfile\n";
+
     open my $dollop_outfile_read, '<', "$dollop_outfile";
     open my $dollop_phylip,       '>', "$dollop_outfile\.phy";
     print $dollop_phylip " 0 $number_of_states\n";    #need to add number of nodes...
-    #open my $report, '>', "$dollop_outfile\_report\.txt";
-    #print $report "Node 1\tNode2\tShared\tLoss\tGain\n";
+
     my $correct_location = 0;
 
     foreach my $line (<$dollop_outfile_read>) {
         chomp($line);
-        next if ( $line =~ m/^[\s\t]*$/ );            # skip blank lines, not needed but speeds up location finding a mite
-        if ($correct_location) {
 
-            # if the lines starts with space followeed by root or a digit, yes or no
-            if ( $line =~ m/\s+(yes|no|maybe|root|\d{1,3})\s+/i ) {
-                $concat_line = "";
-                $count       = 0;
-                my @node = split( /\s+/, $line );     # Split line onf white space
+        # skip blank lines to speed up location finding
+        next if ( $line =~ m/^[\s\t]*$/);
+
+        #if ( $line =~ m/means same as in the node below it on tree/g ) {
+        if ( $line =~ m/( . means same as in the node below it on tree)/g ) { 
+            $correct_location = 1;
+        }
+
+        if ( $correct_location == 1 ) {
+
+            # if the lines starts with root or 2 spaces followed by a digit
+            if ( $line =~ m/^(root|\s{2}\d+)\s+/gism ) {
+
+                print ".";
+                $count = 0;
+
+                # Split line on white space
+                my @node = split( /\s+/, $line );
+
+                # Note the different positions in the array, as we split on space
                 if ( $node[0] eq 'root' ) {
-
-                    #print "$node[0]\t$node[1]\t";     # 3 - 10 are '.01'
-                    print $dollop_phylip "$node[0]\__$node[1]";
-                    #print $report "$node[0]\t$node[1]\t";
+                    print $dollop_phylip "$node[0]\_\_$node[1]";
+                    # Add the first line of dots, 0s and 1s to the concatenated line
+                    $concat_line = $node[3] . $node[4] . $node[5] . $node[6] . $node[7] . $node[8] . $node[9] . $node[10];
                 }
                 else {
-
-                    #print "$node[1]\t$node[2]\t";     # 3 - 10 are '.01'
                     print $dollop_phylip "$node[1]\_\_$node[2]";
-                    #print $report "$node[1]\t$node[2]\t";
+                    # Add the first line of dots, 0s and 1s to the concatenated line
+                    $concat_line = $node[4] . $node[5] . $node[6] . $node[7] . $node[8] . $node[9] . $node[10] . $node[11];
                 }
-
-                # Append the first line of dots, 0s and 1s to the concatenated line
-                $concat_line = $concat_line . $node[3] . $node[4] . $node[5] . $node[6] . $node[7] . $node[8] . $node[9] . $node[10];
             }
             else {
-                $line =~ s/\s//g;    # sack the white spaces
+
+                $line =~ s/\s//g;    # remove white spaces
                 $concat_line = $concat_line . $line;
             }
 
-            # Initiate PERL trickery
-            # Count number of '.', '0', and '1' in concat_line.
-            # '.' = shared, '0' = loss, '1' = gain
-            #my $dot_count  = () = $concat_line =~ m/[\.]/g;
-            #my $one_count  = () = $concat_line =~ m/[1]/g;
-            #my $zero_count = () = $concat_line =~ m/[0]/g;
             if ( $count == $number_of_char_lines ) {
                 print $dollop_phylip "\t$concat_line\n";
-                #print $report "$dot_count\t$zero_count\t$one_count\n";
 
-                #print "$dot_count\t$zero_count\t$one_count\n";
+                # empty the concatened lines for the next set
+                $concat_line    = $EMPTY;
             }
             $count++;
-        }
-        $correct_location = 1 if ( $line =~ m/means same as in the node below it on tree/g );
+        }       
     }
+    print "\n";
 }
